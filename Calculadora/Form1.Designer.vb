2ce041be5197ea117493be508391b09810359be9
Alleph Nogueira <126208329+AllephNogueira@users.noreply.github.com>
2024-09-04 10:14:24 -0300

    Add files via upload

---
+++ Calculadora/Form1.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.ButtonMultiplicacao = New System.Windows.Forms.Button()
+        Me.ButtonSoma = New System.Windows.Forms.Button()
+        Me.ButtonSubtracao = New System.Windows.Forms.Button()
+        Me.ButtonDivisao = New System.Windows.Forms.Button()
+        Me.TextBoxResultado = New System.Windows.Forms.TextBox()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonPonto = New System.Windows.Forms.Button()
+        Me.ButtonCalcular = New System.Windows.Forms.Button()
+        Me.ButtonReset = New System.Windows.Forms.Button()
+        Me.ButtonPorcentagem = New System.Windows.Forms.Button()
+        Me.TextResumo = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 265)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 54)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(91, 265)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 54)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(170, 265)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 54)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(10, 205)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 54)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(91, 205)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 54)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(170, 205)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 54)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(12, 145)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 54)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(91, 145)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 54)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(170, 145)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 54)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'ButtonMultiplicacao
+        '
+        Me.ButtonMultiplicacao.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonMultiplicacao.Location = New System.Drawing.Point(251, 145)
+        Me.ButtonMultiplicacao.Name = "ButtonMultiplicacao"
+        Me.ButtonMultiplicacao.Size = New System.Drawing.Size(75, 54)
+        Me.ButtonMultiplicacao.TabIndex = 9
+        Me.ButtonMultiplicacao.Text = "X"
+        Me.ButtonMultiplicacao.UseVisualStyleBackColor = True
+        '
+        'ButtonSoma
+        '
+        Me.ButtonSoma.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonSoma.Location = New System.Drawing.Point(251, 25)
+        Me.ButtonSoma.Name = "ButtonSoma"
+        Me.ButtonSoma.Size = New System.Drawing.Size(75, 54)
+        Me.ButtonSoma.TabIndex = 9
+        Me.ButtonSoma.Text = "+"
+        Me.ButtonSoma.UseVisualStyleBackColor = True
+        '
+        'ButtonSubtracao
+        '
+        Me.ButtonSubtracao.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonSubtracao.Location = New System.Drawing.Point(251, 85)
+        Me.ButtonSubtracao.Name = "ButtonSubtracao"
+        Me.ButtonSubtracao.Size = New System.Drawing.Size(75, 54)
+        Me.ButtonSubtracao.TabIndex = 10
+        Me.ButtonSubtracao.Text = "-"
+        Me.ButtonSubtracao.UseVisualStyleBackColor = True
+        '
+        'ButtonDivisao
+        '
+        Me.ButtonDivisao.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonDivisao.Location = New System.Drawing.Point(251, 205)
+        Me.ButtonDivisao.Name = "ButtonDivisao"
+        Me.ButtonDivisao.Size = New System.Drawing.Size(75, 54)
+        Me.ButtonDivisao.TabIndex = 11
+        Me.ButtonDivisao.Text = "/"
+        Me.ButtonDivisao.UseVisualStyleBackColor = True
+        '
+        'TextBoxResultado
+        '
+        Me.TextBoxResultado.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBoxResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxResultado.Location = New System.Drawing.Point(10, 60)
+        Me.TextBoxResultado.Margin = New System.Windows.Forms.Padding(0)
+        Me.TextBoxResultado.Multiline = True
+        Me.TextBoxResultado.Name = "TextBoxResultado"
+        Me.TextBoxResultado.Size = New System.Drawing.Size(235, 82)
+        Me.TextBoxResultado.TabIndex = 13
+        Me.TextBoxResultado.Text = "0"
+        '
+        'Button0
+        '
+        Me.Button0.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.Location = New System.Drawing.Point(10, 325)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(75, 54)
+        Me.Button0.TabIndex = 14
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'ButtonPonto
+        '
+        Me.ButtonPonto.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonPonto.Location = New System.Drawing.Point(89, 325)
+        Me.ButtonPonto.Name = "ButtonPonto"
+        Me.ButtonPonto.Size = New System.Drawing.Size(75, 54)
+        Me.ButtonPonto.TabIndex = 15
+        Me.ButtonPonto.Text = "."
+        Me.ButtonPonto.UseVisualStyleBackColor = True
+        '
+        'ButtonCalcular
+        '
+        Me.ButtonCalcular.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.ButtonCalcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonCalcular.Location = New System.Drawing.Point(170, 325)
+        Me.ButtonCalcular.Name = "ButtonCalcular"
+        Me.ButtonCalcular.Size = New System.Drawing.Size(75, 54)
+        Me.ButtonCalcular.TabIndex = 16
+        Me.ButtonCalcular.Text = "="
+        Me.ButtonCalcular.UseVisualStyleBackColor = False
+        '
+        'ButtonReset
+        '
+        Me.ButtonReset.BackColor = System.Drawing.Color.OrangeRed
+        Me.ButtonReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonReset.Location = New System.Drawing.Point(251, 325)
+        Me.ButtonReset.Name = "ButtonReset"
+        Me.ButtonReset.Size = New System.Drawing.Size(75, 54)
+        Me.ButtonReset.TabIndex = 17
+        Me.ButtonReset.Text = "C"
+        Me.ButtonReset.UseVisualStyleBackColor = False
+        '
+        'ButtonPorcentagem
+        '
+        Me.ButtonPorcentagem.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonPorcentagem.Location = New System.Drawing.Point(251, 265)
+        Me.ButtonPorcentagem.Name = "ButtonPorcentagem"
+        Me.ButtonPorcentagem.Size = New System.Drawing.Size(75, 54)
+        Me.ButtonPorcentagem.TabIndex = 18
+        Me.ButtonPorcentagem.Text = "%"
+        Me.ButtonPorcentagem.UseVisualStyleBackColor = True
+        '
+        'TextResumo
+        '
+        Me.TextResumo.BackColor = System.Drawing.SystemColors.Window
+        Me.TextResumo.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextResumo.ForeColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextResumo.Location = New System.Drawing.Point(10, 25)
+        Me.TextResumo.Margin = New System.Windows.Forms.Padding(0)
+        Me.TextResumo.Multiline = True
+        Me.TextResumo.Name = "TextResumo"
+        Me.TextResumo.Size = New System.Drawing.Size(235, 42)
+        Me.TextResumo.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(330, 387)
+        Me.Controls.Add(Me.TextResumo)
+        Me.Controls.Add(Me.ButtonPorcentagem)
+        Me.Controls.Add(Me.ButtonReset)
+        Me.Controls.Add(Me.ButtonCalcular)
+        Me.Controls.Add(Me.ButtonPonto)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.TextBoxResultado)
+        Me.Controls.Add(Me.ButtonDivisao)
+        Me.Controls.Add(Me.ButtonSubtracao)
+        Me.Controls.Add(Me.ButtonSoma)
+        Me.Controls.Add(Me.ButtonMultiplicacao)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents ButtonMultiplicacao As Button
+    Friend WithEvents ButtonSoma As Button
+    Friend WithEvents ButtonSubtracao As Button
+    Friend WithEvents ButtonDivisao As Button
+    Friend WithEvents TextBoxResultado As TextBox
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonPonto As Button
+    Friend WithEvents ButtonCalcular As Button
+    Friend WithEvents ButtonReset As Button
+    Friend WithEvents ButtonPorcentagem As Button
+    Friend WithEvents TextResumo As TextBox
+End Class
---
+++ Calculadora/Form1.vb
@@ -0,0 +1,176 @@
+﻿Public Class Form1
+
+    ' Calculos
+    Dim operacao As String
+    Dim valorResultado As Nullable(Of Double) = Nothing 'Iniciando com valor nulo
+    Dim valor2 As Nullable(Of Double) = Nothing 'Iniciando com valor nulo
+    Dim sePresionarOperador As Boolean
+
+    ' Validar as operacoes
+
+    Private Sub ValidarOperacao()
+        sePresionarOperador = True
+        valor2 = Val(TextBoxResultado.Text.Replace(",", ".")) 'Convertendo para um numero.
+        If valorResultado IsNot Nothing Then
+            Select Case operacao
+                Case "+"
+                    valorResultado = valorResultado + valor2
+                Case "-"
+                    valorResultado -= valor2
+                Case "*"
+                    valorResultado *= valor2
+                Case "/"
+                    If valor2 > 0 Then
+                        valorResultado /= valor2
+                    Else
+                        TextBoxResultado.Text = "Não é possível dividir por zero"
+                        Return
+                    End If
+                Case "%"
+                    valorResultado *= valor2 / 100
+            End Select
+            TextBoxResultado.Text = valorResultado
+        Else
+            valorResultado = valor2
+
+        End If
+
+    End Sub
+
+    Private Sub Calcular()
+        If sePresionarOperador = True Then
+            TextBoxResultado.Text = ""
+            sePresionarOperador = False
+        ElseIf TextBoxResultado.Text = "0" Then
+            TextBoxResultado.Text = ""
+        End If
+    End Sub
+
+
+
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        Calcular()
+        TextBoxResultado.Text &= "0"
+        TextResumo.Text &= "0"
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Calcular()
+        TextBoxResultado.Text &= "1"
+
+        TextResumo.Text &= "1"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Calcular()
+        TextBoxResultado.Text &= "2"
+
+        TextResumo.Text &= "2"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Calcular()
+        TextBoxResultado.Text &= "3"
+        TextResumo.Text &= "3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Calcular()
+        TextBoxResultado.Text &= "4"
+        TextResumo.Text &= "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Calcular()
+        TextBoxResultado.Text &= "5"
+        TextResumo.Text &= "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Calcular()
+        TextBoxResultado.Text &= "6"
+        TextResumo.Text &= "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Calcular()
+        TextBoxResultado.Text &= "7"
+        TextResumo.Text &= "7"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Calcular()
+        TextBoxResultado.Text &= "8"
+        TextResumo.Text &= "8"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Calcular()
+        TextBoxResultado.Text &= "9"
+        TextResumo.Text &= "9"
+    End Sub
+
+    Private Sub ButtonPonto_Click(sender As Object, e As EventArgs) Handles ButtonPonto.Click
+        Calcular()
+
+        If Not TextBoxResultado.Text.Contains(",") Then
+            TextBoxResultado.Text &= ","
+            TextResumo.Text &= ","
+        End If
+
+    End Sub
+
+    Private Sub ButtonSoma_Click(sender As Object, e As EventArgs) Handles ButtonSoma.Click
+        ValidarOperacao()
+        operacao = "+"
+        TextResumo.Text &= "+"
+
+    End Sub
+
+    Private Sub ButtonSubtracao_Click(sender As Object, e As EventArgs) Handles ButtonSubtracao.Click
+        ValidarOperacao()
+        operacao = "-"
+        TextResumo.Text &= "-"
+    End Sub
+
+    Private Sub ButtonMultiplicacao_Click(sender As Object, e As EventArgs) Handles ButtonMultiplicacao.Click
+        ValidarOperacao()
+        operacao = "*"
+        TextResumo.Text &= "*"
+
+    End Sub
+
+    Private Sub ButtonDivisao_Click(sender As Object, e As EventArgs) Handles ButtonDivisao.Click
+        ValidarOperacao()
+        operacao = "/"
+        TextResumo.Text &= "/"
+
+    End Sub
+
+    Private Sub ButtonPorcentagem_Click(sender As Object, e As EventArgs) Handles ButtonPorcentagem.Click
+        ValidarOperacao()
+        operacao = "%"
+        TextResumo.Text &= "%"
+
+    End Sub
+
+    Private Sub ButtonCalcular_Click(sender As Object, e As EventArgs) Handles ButtonCalcular.Click
+        ValidarOperacao()
+        operacao = ""
+    End Sub
+
+    Private Sub ButtonReset_Click(sender As Object, e As EventArgs) Handles ButtonReset.Click
+        TextBoxResultado.Text = "0"
+        valor2 = Nothing
+        valorResultado = Nothing
+        TextResumo.Text = ""
+    End Sub
+
+    Private Sub TextBoxResultado_TextChanged(sender As Object, e As EventArgs) Handles TextBoxResultado.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextResumo.TextChanged
+        ' Aqui é onde vamos por o resumo das operacoes.
+    End Sub
+End Class
